c2e4be892c214edee1b22de292994f5a13cda764
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2017-01-18 21:07:48 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+
+  req.addEventListener("load", showRepositories)
+
+
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  console.log(this.responseText)
+}
